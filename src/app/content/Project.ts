8fbf6208c46ad102fd70eba41ff84c781e6efbc8
Babken <babkenboyaxchyan@gmail.com>
2021-12-03 10:16:24 +0400

    side bar

---
+++ src/app/content/Project.ts
@@ -0,0 +1,29 @@
+import {stringify} from "querystring";
+
+type InterventionCountry = {
+  id:number,
+  name:string
+}
+
+type WorkflowState = {
+  id:number,
+  name:string
+}
+type UpdatedUser = {
+  id:number,
+  name:string
+}
+
+
+class Project{
+
+  constructor(
+    private interventionCode:string,
+    private shortName:string,
+    private title:string,
+    private interventionCountryId:number,
+    private workflowStateId:number,
+    private updatedUserId:number,
+    private description:string,
+  ) {}
+}
